05482af538af9e65b9980da990137f051c990c6d
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-08 12:34:16 +0530

    Episode-02 Igniting App

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const root = document.getElementById('root');
 root.innerHTML = "Hello World from Javascript";
 
 const heading = React.createElement("h1",{},"Namaste From React");
 
-const reactRoot = ReactDOM.createRoot(document.getElementById('root1'));
+const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 reactRoot.render(heading);
