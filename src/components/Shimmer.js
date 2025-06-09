8480c4b9ac2f2cb8058e757f85ea198ac63972cd
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-09 17:01:37 +0530

    Episode -06 Exploring World

---
+++ src/components/Shimmer.js
@@ -0,0 +1,30 @@
+const Shimmer = () => {
+
+    console.log("Shimmer Called..");
+    
+    // return <div>Loading ....</div>
+    return (
+      <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    );
+  };
+  
+  export default Shimmer;
