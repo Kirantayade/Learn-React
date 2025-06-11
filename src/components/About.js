9e68dd7dfc4f0ea9b8d8d60f3a315e9cb7dab003
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-11 17:28:11 +0530

    Episode - 07 Finding Path

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = ()=>{
+    return(
+        <div>
+            <h1>About</h1>
+            <h2> This is my Learn React About Page</h2>
+        </div>
+        
+    )
+}
+
+export default About;
---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = ()=> {
+    const error = useRouteError();
+    console.log(error);
+    return(
+        <div>
+            <h1>This is Error PAge</h1>
+            <h2>Something Went Wrong!!!</h2>
+            <h3>{error.status}:{error.statusText}</h3>
+        </div>
+    )
+}
+
+export default Error;
