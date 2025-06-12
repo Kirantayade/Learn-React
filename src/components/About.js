24847eb90e2bdcf0eb29bd349461b1e606b989ca
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-12 21:50:35 +0530

    Episode- 08 part -02 get Classy

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,6 @@ const About = ()=>{
         <div className="user-card">
             <h1>About</h1>
             <h2> This is my Learn React About Page</h2>
-
-            <User name ={"Kiran Tayade (Function)"} location={"Pune"} contact ={"L042269"}/>
             
             <UserClass name ={"Kiran Tayade (Class)"} location={"Pune Class"} contact ={"L042269 Class"}/>
         </div>
