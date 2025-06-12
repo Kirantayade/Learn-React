ac4a7c839895cc85db14d1566849ad72c09c445a
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-12 20:22:19 +0530

    Episode- 08 part -01 get Classy

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = ()=>{
     return(
-        <div>
+        <div className="user-card">
             <h1>About</h1>
             <h2> This is my Learn React About Page</h2>
+
+            <User name ={"Kiran Tayade (Function)"} location={"Pune"} contact ={"L042269"}/>
+            
+            <UserClass name ={"Kiran Tayade (Class)"} location={"Pune Class"} contact ={"L042269 Class"}/>
         </div>
         
     )
