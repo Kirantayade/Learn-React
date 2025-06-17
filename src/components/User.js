62ddb909e479e22cf69ead33e98d1be31af83a54
Kiran Tayade - Network <kiran.tayade@network.lilly.com>
2025-06-17 23:07:09 +0530

    Episode -08 Final Get Classy

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
-const User = (props)=>{
-    const [count] = useState (0);
-    const [count2] = useState (1);
+const User = ({ name }) => {
+  const [count, setCount] = useState(0);
+  const [count2] = useState(1);
 
-    return (
-        <div className="user-card">
-            <h1>Count ={count}</h1>
-            <h1>Count2 ={count2}</h1>
-            <h2>Name:{props.name}</h2>
-            <h3>Location:{props.locatiom}</h3>
-            <h4>Contact:{props.contact}</h4>
-        </div>
-    );
-}
+  useEffect(() => {
+    // Api Calls
+  }, []);
+
+  return (
+    <div className="user-card">
+      <h1>Count = {count}</h1>
+      <h1>Count2 = {count2}</h1>
+      <h2>Name: {name}</h2>
+      <h3>Location: Dehradun</h3>
+      <h4>Contact: @akshaymarch7</h4>
+    </div>
+  );
+};
 
 export default User;
